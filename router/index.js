9ee49efb1306aece60be0f0398de1fd96837f21a
Maulana Ibrahim <maulputra09@gmail.com>
2023-11-22 15:39:00 +0700

    add more socket feature

--- router/index.js
+++ router/index.js
@@ -5,12 +5,14 @@ const Controller = require('../Controllers/controller')
 
 router.post("/create-room", Controller.createRoom)
 router.patch("/join-room/:roomId", Controller.joinRoom)
-// router.delete("/room/:id", Controller.destroyRoom)
+
 router.get("/my-profile", Controller.myProfile)
 router.get("/rooms", Controller.getAllRoom)
 router.get("/leaderboard", Controller.leaderboard)
 router.get("/rooms/:roomId", Controller.waitingRoom)
 
 router.patch("/leave-room/:roomId", Controller.leaveRoom)
+
+router.delete("/room/:roomId", Controller.destroyRoom)
 
 module.exports = router;
